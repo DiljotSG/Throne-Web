f3b9e1f1e1c0eca61b02ca3caa037a3e79e302a4
James Klassen <jameswklassen@gmail.com>
2020-02-02 20:31:09 -0600

    Fetch locations from dev endpoint

--- src/pages/NearMe.js
+++ src/pages/NearMe.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NearMe = () => (
-  <>
-    <h1>Near Me</h1>
-  </>
-);
+const NearMe = (props) => {
+  const { locations } = props;
+  return (
+    <>
+      <h1>Near Me</h1>
+      { locations.length > 0 ? locations.map((location) => (<li key={location}>{location}</li>)) : 'Loading locations....' }
+    </>
+  );
+};
+
+NearMe.propTypes = {
+  locations: PropTypes.instanceOf(Array),
+};
+
+NearMe.defaultProps = {
+  locations: [],
+};
+
 
 export default NearMe;
--- src/pages/NearMe.test.js
+++ src/pages/NearMe.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import NearMe from './NearMe';
 
 describe('NearMe', () => {
   it('Renders the "Near me" page', () => {
-    const component = shallow(<NearMe />);
+    const component = mount(<NearMe />);
 
-    expect(component.text()).toEqual('Near Me');
+    expect(component.find('h1').text()).toEqual('Near Me');
+  });
+
+  it('Displays a list of locations', () => {
+    const locations = ['Location1', 'Location2'];
+    const component = mount(<NearMe locations={locations} />);
+
+    expect(component.find('li')).toHaveLength(2);
+    expect(component.find('li').first().text()).toEqual('Location1');
+    expect(component.find('li').at(1).text()).toEqual('Location2');
   });
 });
