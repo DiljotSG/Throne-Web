77a53f050fd232a05b816ddc00e97af30bde30c9
Aaron M. Roller <AaronMichaelRoller@gmail.com>
2020-02-16 12:21:10 -0600

    Adds wrapper functions for Throne API endpoints (#38)
    * Adds wrapper functions for Throne api endpoints
    
    * Normalize function calls
    
    * Normalize function names
    
    * Added more descriptive function names
    
    * Corrected error

---
+++ src/api/throneApi.js
@@ -0,0 +1,69 @@
+class ThroneApi {
+  static async getEndpoint(url) {
+    try {
+      return await fetch(url);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static createEndpointURL(relativeURL) {
+    return new URL(relativeURL, `${process.env.REACT_APP_API_URL}`);
+  }
+
+  static async getWashrooms() {
+    const url = this.createEndpointURL('washrooms');
+    return this.getEndpoint(url);
+  }
+
+  static async getWashroom(id) {
+    const url = this.createEndpointURL(`washrooms/${id}`);
+    return this.getEndpoint(url);
+  }
+
+  static async getReviewsForWashroom(id) {
+    const url = this.createEndpointURL(`washrooms/${id}/reviews`);
+    return this.getEndpoint(url);
+  }
+
+  static async getReview(id) {
+    const url = this.createEndpointURL(`reviews/${id}`);
+    return this.getEndpoint(url);
+  }
+
+  static async getBuilding(id) {
+    const url = this.createEndpointURL(`buildings/${id}`);
+    return this.getEndpoint(url);
+  }
+
+  static async getBuildings(location, maxResults, amenities, radius) {
+    const url = this.createEndpointURL('buildings');
+    url.searchParams.append('location', location);
+    url.searchParams.append('maxResults', maxResults);
+    url.searchParams.append('radius', radius);
+    url.searchParams.append('amenities', amenities);
+    return this.getEndpoint(url);
+  }
+
+  static async getWashroomsForBuilding(id) {
+    const url = this.createEndpointURL(`buildings/${id}/washrooms`);
+    return this.getEndpoint(url);
+  }
+
+  static async getUser(id) {
+    const url = this.createEndpointURL(`users/${id}`);
+    return this.getEndpoint(url);
+  }
+
+  static async getReviewsForUser(id) {
+    const url = this.createEndpointURL(`users/${id}/reviews`);
+    return this.getEndpoint(url);
+  }
+
+  static async getFavoritesForUser(id) {
+    const url = this.createEndpointURL(`users/${id}/favorites`);
+    return this.getEndpoint(url);
+  }
+}
+
+export default ThroneApi;
