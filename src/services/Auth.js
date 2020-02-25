53ad2cf097f41680de7418e0c056bba8c8ab4d5e
James Klassen <jameswklassen@gmail.com>
2020-02-25 13:20:00 -0600

    Fix token refresh on initial page load

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -8,6 +8,8 @@ export default class Auth {
   static refreshAttempted = false;
 
   static async attemptLogin() {
+    await Auth.refreshLogin();
+
     const params = new URLSearchParams(window.location.search);
 
     if (!Auth.authenticated()) {
