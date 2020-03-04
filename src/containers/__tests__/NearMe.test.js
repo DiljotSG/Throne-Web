96c9daed2a8931577b4bdce4e38c6105150233c0
Adrian Polimeni <adrianpolimeni@gmail.com>
2020-03-04 10:44:20 -0600

    Lint fix

--- src/containers/__tests__/NearMe.test.js
+++ src/containers/__tests__/NearMe.test.js
@@ -61,8 +61,8 @@ describe('NearMe', () => {
     await act(async () => {
       const component = mount(<Router><NearMe store={store} /></Router>);
       expect(component.find('li.near-me-list-item')).toHaveLength(2);
-      expect(component.find('li.near-me-list-item').first().text()).toBe("Washroom 1");
-      expect(component.find('li.near-me-list-item').at(1).text()).toBe("Washroom 2");
+      expect(component.find('li.near-me-list-item').first().text()).toBe('Washroom 1');
+      expect(component.find('li.near-me-list-item').at(1).text()).toBe('Washroom 2');
     });
   });
 });
