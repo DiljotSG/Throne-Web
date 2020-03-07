5b2f3f91ab9531aa929bc8c60f1418e18095b67d
Tyler Loewen <tylerscottloewen@gmail.com>
2020-03-06 20:05:00 -0600

    Change ratings to display half stars

--- src/utils/NumUtils.js
+++ src/utils/NumUtils.js
@@ -1,4 +1,6 @@
-const roundToHalf = ({ value }) => {
+export const roundToHalf = (value) => {
   const valueFloor = Math.floor(value);
   return valueFloor + Math.round(value - valueFloor) / 2;
 };
+
+export default roundToHalf;
