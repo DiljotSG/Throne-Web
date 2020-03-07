5086de34e0a6a6e7448aabc23b502d4e0c3cc4bb
James Klassen <jameswklassen@gmail.com>
2020-03-07 10:47:44 -0600

    Display buildings on NearMe page

--- src/containers/NearMe.js
+++ src/containers/NearMe.js
@@ -4,21 +4,33 @@ import {
   Typography,
   List,
   Icon,
+  Tabs,
 } from 'antd';
 import { connect } from 'react-redux';
 import { getWashrooms } from '../actions/washroomActions';
+import { getBuildings } from '../actions/buildingActions';
+
 import { WashroomListItem } from '../components';
 
-import './NearMe.css';
-
 const { Title } = Typography;
+const { TabPane } = Tabs;
 
 class NearMe extends Component {
   componentDidMount() {
-    const { washrooms } = this.props;
+    const { washrooms, buildings } = this.props;
+
     if (washrooms.length === 0) {
       this.getWashrooms();
     }
+    if (buildings.length === 0) {
+      this.getBuildings();
+    }
+  }
+
+  getBuildings = () => {
+    const { getBuildings } = this.props; // eslint-disable-line no-shadow
+
+    getBuildings();
   }
 
   getWashrooms = () => {
@@ -28,54 +40,109 @@ class NearMe extends Component {
   }
 
   render() {
-    const { washrooms, isFetching } = this.props;
+    const {
+      washrooms,
+      buildings,
+      washroomsFetching,
+      buildingsFetching,
+    } = this.props;
 
     return (
       <>
         <Icon type="environment" className="icon-title" />
         <Title>Near Me</Title>
-        <List
-          className="near-me-list"
-          loading={isFetching}
-          bordered
-          dataSource={washrooms}
-          renderItem={(item) => (
-            <List.Item
-              className="near-me-list-item"
-              key={item.id}
-            >
-              <WashroomListItem item={item} />
-            </List.Item>
-          )}
-        />
+
+        <Tabs
+          defaultActiveKey="1"
+          size="large"
+          animated={false}
+        >
+          <TabPane
+            tab="Buildings"
+            key="1"
+          >
+            <List
+              className="near-me-list"
+              loading={buildingsFetching}
+              bordered
+              dataSource={buildings}
+              renderItem={(item) => (
+                <List.Item
+                  className="near-me-list-item"
+                  key={item.id}
+                >
+                  {item.title}
+                </List.Item>
+              )}
+            />
+          </TabPane>
+          <TabPane
+            tab="Washrooms"
+            key="2"
+          >
+            <List
+              className="near-me-list"
+              loading={washroomsFetching}
+              bordered
+              dataSource={washrooms}
+              renderItem={(item) => (
+                <List.Item
+                  className="near-me-list-item"
+                  key={item.id}
+                >
+                  <WashroomListItem item={item} />
+                </List.Item>
+              )}
+            />
+          </TabPane>
+        </Tabs>
       </>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  const { washrooms, isFetching, status } = state.washroomReducer;
+  const {
+    washrooms, isFetching:
+    washroomsFetching,
+    status: washroomsStatus,
+  } = state.washroomReducer;
+
+  const {
+    buildings, isFetching:
+    buildingsFetching,
+    status: buildingsStatus,
+  } = state.buildingReducer;
 
   return {
-    status,
-    isFetching,
     washrooms,
+    washroomsFetching,
+    washroomsStatus,
+    buildings,
+    buildingsFetching,
+    buildingsStatus,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   getWashrooms: () => dispatch(getWashrooms()),
+  getBuildings: () => dispatch(getBuildings()),
 });
 
 NearMe.propTypes = {
-  getWashrooms: PropTypes.func.isRequired,
   washrooms: PropTypes.instanceOf(Array),
-  isFetching: PropTypes.bool,
+  buildings: PropTypes.instanceOf(Array),
+  getWashrooms: PropTypes.func.isRequired,
+  getBuildings: PropTypes.func.isRequired,
+  washroomsFetching: PropTypes.bool,
+  buildingsFetching: PropTypes.bool,
 };
 
 NearMe.defaultProps = {
   washrooms: [],
-  isFetching: false,
+  buildings: [],
+  washroomsFetching: false,
+  buildingsFetching: false,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NearMe);
