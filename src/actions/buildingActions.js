32d5626c451f0cf3a997dd0261f5d26bdb1d2dad
Tyler Loewen <tylerscottloewen@gmail.com>
2020-03-08 15:55:12 -0500

    Add logic for getting single building plus tests

--- src/actions/buildingActions.js
+++ src/actions/buildingActions.js
@@ -17,6 +17,20 @@ export const receiveBuildings = (response, status) => (
   }
 );
 
+export function requestBuilding() {
+  return {
+    type: actions.REQUEST_BUILDING,
+  };
+}
+
+export const receiveBuilding = (response, status) => (
+  {
+    type: actions.RECEIVE_BUILDING,
+    building: response,
+    status,
+  }
+);
+
 export function getBuildings() {
   return async function fetchBuildingsAsync(dispatch) {
     dispatch(requestBuildings());
@@ -25,6 +39,25 @@ export function getBuildings() {
       if (response.ok) {
         response.json().then((buildings) => {
           dispatch(receiveBuildings(buildings, response.status));
+        });
+      } else {
+        dispatch(failure(response.status));
+      }
+    }).catch((error) => {
+      dispatch(failure());
+      throw (error);
+    });
+  };
+}
+
+export function getBuilding(id) {
+  return async function fetchBuildingsAsync(dispatch) {
+    dispatch(requestBuilding());
+
+    return throneApi.getBuilding(id).then((response) => {
+      if (response.ok) {
+        response.json().then((building) => {
+          dispatch(receiveBuilding(building, response.status));
         });
       } else {
         dispatch(failure(response.status));
