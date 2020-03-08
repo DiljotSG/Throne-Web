660297c75c9bf91909e77b20c4d7e76ff13f9d05
James Klassen <13937038+jameswklassen@users.noreply.github.com>
2020-03-08 14:26:24 -0500

    Display username on Profile page (#113)

--- src/containers/__tests__/Profile.test.js
+++ src/containers/__tests__/Profile.test.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
+import fetchMock from 'fetch-mock';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../../reducers';
 import Profile from '../Profile';
 
-describe('Profile', () => {
-  it('Renders the Profile page', () => {
-    const component = mount(<Profile />);
+export default function setupStore(initialState) {
+  return createStore(rootReducer, { ...initialState }, applyMiddleware(thunk));
+}
 
-    expect(component.find('Title').text()).toEqual('User Name');
+const store = setupStore({});
+
+fetchMock.get('https://testapi.com/users',
+  {
+    id: 1,
+    preferences: {
+      gender: 'undefined',
+      main_floor_access: 0,
+      wheelchair_accessible: 0,
+    },
+    profile_picture: 'default',
+    username: 'twophase',
+  });
+
+describe('Profile', () => {
+  it('Renders the Profile page', async () => {
+    let component;
+    await act(async () => {
+      component = mount(
+        <Router>
+          <Profile store={store} />
+        </Router>,
+      );
+    });
+    component.update();
+    expect(component.find('Title').text()).toEqual('twophase');
   });
 });
