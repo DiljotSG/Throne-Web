05d8a668f7b5a0204fc30aaae397a8dc5626c18b
James Klassen <jameswklassen@gmail.com>
2020-03-12 12:29:19 -0500

    Generalize API request sender

--- src/api/throneApi.js
+++ src/api/throneApi.js
@@ -1,81 +1,30 @@
 import Auth from '../services/Auth';
+import { GET, POST, DELETE } from '../constants/HTTPTypes';
 
 class ThroneApi {
-  static async getEndpoint(url) {
+  static async accessEndpoint(method, url, body = null) {
     const accessToken = localStorage.getItem('accessToken');
-    let response = await this.getRequest(url, accessToken);
+    let response = await this.request(method, url, accessToken, body);
 
     if (response.status === 401) {
       await Auth.refreshLogin();
-      response = await this.getRequest(url, accessToken);
+      response = await this.request(method, url, accessToken, body);
     }
 
     return response;
   }
 
-  static async postEndpoint(url, data) {
-    const accessToken = localStorage.getItem('accessToken');
-    let response = await this.postRequest(url, accessToken, data);
-
-    if (response.status === 401) {
-      await Auth.refreshLogin();
-      response = await this.postRequest(url, accessToken, data);
-    }
-
-    return response;
-  }
-
-  static async deleteEndpoint(url, data) {
-    const accessToken = localStorage.getItem('accessToken');
-    let response = await this.deleteRequest(url, accessToken, data);
-
-    if (response.status === 401) {
-      await Auth.refreshLogin();
-      response = await this.deleteRequest(url, accessToken, data);
-    }
-
-    return response;
-  }
-
-  static async getRequest(url, accessToken) {
+  static async request(method, url, accessToken, body) {
     try {
       return await fetch(url, {
+        method,
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
+        body: body ? JSON.stringify(body) : null,
       });
     } catch (error) {
       return error;
-    }
-  }
-
-  static async postRequest(url, accessToken, data) {
-    try {
-      return await fetch(url, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-    } catch (error) {
-      return error;
-    }
-  }
-
-  static async deleteRequest(url, accessToken, data) {
-    try {
-      return await fetch(url, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-    } catch (error) {
-      return (error);
     }
   }
 
@@ -85,27 +34,27 @@ class ThroneApi {
 
   static async getWashrooms() {
     const url = this.createEndpointURL('washrooms');
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getWashroom(id) {
     const url = this.createEndpointURL(`washrooms/${id}`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getReviewsForWashroom(id) {
     const url = this.createEndpointURL(`washrooms/${id}/reviews`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getReview(id) {
     const url = this.createEndpointURL(`reviews/${id}`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getBuilding(id) {
     const url = this.createEndpointURL(`buildings/${id}`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getBuildings(location, maxResults, amenities, radius) {
@@ -114,37 +63,37 @@ class ThroneApi {
     url.searchParams.append('maxResults', maxResults);
     url.searchParams.append('radius', radius);
     url.searchParams.append('amenities', amenities);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getWashroomsForBuilding(id) {
     const url = this.createEndpointURL(`buildings/${id}/washrooms`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getUser(id) {
     const url = this.createEndpointURL(`users/${id}`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getCurrentUser() {
     const url = this.createEndpointURL('users');
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async getReviewsForUser(id) {
     const url = this.createEndpointURL(`users/${id}/reviews`);
-    return this.getEndpoint(url);
+    return this.accessEndpoint(GET, url);
   }
 
   static async addFavorite(id) {
     const url = this.createEndpointURL('users/favorites');
-    return this.postEndpoint(url, { washroom_id: id });
+    return this.accessEndpoint(POST, url, { washroom_id: id });
   }
 
   static async removeFavorite(id) {
     const url = this.createEndpointURL('users/favorites');
-    return this.deleteEndpoint(url, { washroom_id: id });
+    return this.accessEndpoint(DELETE, url, { washroom_id: id });
   }
 }
 
