ae6b465888ce6d62c4fd0088323c995b84aa2b3d
Diljot Garcha <diljotsg@gmail.com>
2020-03-14 12:18:55 -0500

    Lint fix

--- src/utils/DisplayUtils.js
+++ src/utils/DisplayUtils.js
@@ -175,9 +175,7 @@ export const ratingAsEmoji = (value) => {
   }
 };
 
-export const displayDistance = (distance) => {
-  return distance > 1000 ? `${(distance / 1000).toFixed(1)} km` : `${(distance).toFixed(2)} m`;
-}
+export const displayDistance = (distance) => (distance > 1000 ? `${(distance / 1000).toFixed(1)} km` : `${(distance).toFixed(2)} m`);
 
 export default {
   genderAsString,
