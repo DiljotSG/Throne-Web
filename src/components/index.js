827b57ff9ddd7a477a86f4884dd2979f88ad7e5d
James Klassen <jameswklassen@gmail.com>
2020-03-14 17:29:43 -0500

    Extract washroom ratings into a component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Nav from './Nav';
 import Login from './Login';
 import WashroomListItem from './WashroomListItem';
+import WashroomRatings from './WashroomRatings';
 
-export { Nav, Login, WashroomListItem };
+export {
+  Nav, Login, WashroomListItem, WashroomRatings,
+};
