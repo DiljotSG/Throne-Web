7ad00affc6b9aad43363dfb97aa2a848c1964542
Diljot Garcha <diljotsg@gmail.com>
2020-03-26 23:23:54 -0500

    Start adding local settings

---
+++ src/constants/PersistentSettings.js
@@ -0,0 +1,15 @@
+export const DEFAULT_TERM = 'washroom';
+export const PREFERRED_TERM_OPTIONS = [
+    'john',
+    'crapper',
+    'latrine',
+    DEFAULT_TERM,
+    'bathroom',
+    'toilet',
+    'restroom',
+    'powder room',
+    'comfort station',
+    'water closet',
+    'loo',
+    'can'
+];
--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { Typography, Icon } from 'antd';
+import PropTypes from 'prop-types';
+import { Typography, Icon, Select, Button } from 'antd';
+import { PREFERRED_TERM_OPTIONS, DEFAULT_TERM } from '../constants/PersistentSettings';
+
+import './Settings.css';
 
 const { Title } = Typography;
+const { Option } = Select;
 
+// This needs to be made into a component somehow
 const Settings = () => (
   <>
     <Icon type="setting" className="icon-title" />
     <Title>Settings</Title>
+    <Select
+      style={{ width: 200 }}
+      onChange={console.log}
+      placeholder="Preferred Terminology"
+    >
+      {
+        PREFERRED_TERM_OPTIONS.map(
+          (object) => (
+                <Option 
+                  key={object}
+                  value={object}
+                >
+                  {object}
+                </Option>
+            )
+          )
+        }
+    </Select>
+    <div>
+      <Button
+        type="primary"
+        className="save-terminology-button"
+        onClick={(value) => (console.log(value))}
+      >
+        Save
+      </Button>
+    </div>
   </>
 );
+
+Settings.propTypes = {
+  washrooms: PropTypes.instanceOf(String),
+};
+
+Settings.defaultProps = {
+  terminology: DEFAULT_TERM
+};
 
 export default Settings;
