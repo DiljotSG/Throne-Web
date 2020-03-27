8a52c8b7da6908bf4475e7acfbb8eda1c125697f
Adrian Polimeni <adrianpolimeni@gmail.com>
2020-03-27 18:10:55 -0500

    Update Messages.js

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,4 +1,1 @@
 export const ERROR_REVIEW_EMPTY_RATINGS = 'All ratings must have values';
-export const ERROR_EMPTY_FLOOR = 'Floor cannot be empty';
-export const ERROR_EMPTY_STALL_COUNT = 'Stall count cannot be empty';
-export const ERROR_EMPTY_URINAL_COUNT = 'Urinal count cannot be empty';
