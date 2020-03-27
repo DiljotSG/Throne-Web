5630c74cc0e42191cf1e85afcfce724d0f982cf9
Diljot Garcha <diljotsg@gmail.com>
2020-03-26 21:48:08 -0500

    Combine profile and logout in nav / Add settings to profile page (#244)
    * Combine profile, settings and logout in nav
    
    * Move settings cog to profile page
    
    * Fix tests
    
    * Update src/containers/Profile.js
    
    Co-Authored-By: Tyler Loewen <tylerscottloewen@gmail.com>
    
    * Update src/components/Nav.js
    
    Co-Authored-By: Tyler Loewen <tylerscottloewen@gmail.com>
    
    * Update src/containers/Profile.js
    
    Co-Authored-By: James Klassen <13937038+jameswklassen@users.noreply.github.com>
    
    * Fix lint errors
    
    * PR Fixes
    
    Co-authored-by: Tyler Loewen <tylerscottloewen@gmail.com>
    Co-authored-by: James Klassen <13937038+jameswklassen@users.noreply.github.com>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, NavLink } from 'react-router-dom';
 
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu } from 'antd';
 
 import './Nav.css';
 
+const { SubMenu } = Menu;
 const { Header } = Layout;
 
 const selectedKeys = (pathname) => (
@@ -23,9 +24,13 @@ const Nav = ({ location, logout }) => (
     >
       <Menu.Item key="/"><NavLink to="/">Near Me</NavLink></Menu.Item>
       <Menu.Item key="/map"><NavLink to="/map">Map</NavLink></Menu.Item>
-      <Menu.Item key="/profile"><NavLink to="/profile">Profile</NavLink></Menu.Item>
-      <Menu.Item key="/settings"><NavLink to="/settings"><Icon type="setting" /></NavLink></Menu.Item>
-      <Menu.Item key="/logout" onClick={logout}>Logout</Menu.Item>
+      <SubMenu
+        title="Me"
+        key="/profile"
+      >
+        <Menu.Item key="/profile"><NavLink to="/profile">Profile</NavLink></Menu.Item>
+        <Menu.Item key="/logout" onClick={logout}>Logout</Menu.Item>
+      </SubMenu>
     </Menu>
   </Header>
 );
